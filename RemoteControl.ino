31b1081840e5f6c616df900baded135d05300e76
Luke Fisk-Lennon <luke.fisklennon@gmail.com>
2019-01-27 16:38:51 +1100

    Clarity

--- RemoteControl.ino
+++ RemoteControl.ino
@@ -16,19 +16,23 @@ void setup() {
 }
 
 void loop() {
-  int state = digitalRead(sensorPin);
-  remoteUpdate(!state);
-  if (state == FAR) {
-    float left = joystickY + joystickX;
-    float right = joystickY - joystickX;
-    if (left > 1) left = 1;
-    if (left < -1) left = -1;
-    if (right > 1) right = 1;
-    if (right < -1) right = -1;
-    leftMotor(left * 1024);
-    rightMotor(right * 1024);
-  } else {
-    leftMotor(-1024);
-    rightMotor(-1024);
+  int reading = digitalRead(sensorPin);
+  int vibrate = 0;
+  if (reading == CLOSE) {
+    vibrate = 1;
   }
+  remoteUpdate(vibrate);
+  float forwards = joystickY;
+  float sideways = joystickX;
+  if (reading == CLOSE && forwards > 0) {
+    forwards = 0;
+  }
+  float left = forwards + sideways;
+  float right = forwards - sideways;
+  if (left > 1) left = 1;
+  if (left < -1) left = -1;
+  if (right > 1) right = 1;
+  if (right < -1) right = -1;
+  leftMotor(left * 1024);
+  rightMotor(right * 1024);
 }
